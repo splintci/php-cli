8ee3392d701eefceb8abc9a6742adf3ccb057a48
Francis Ilechukwu <francis94c@gmail.com>
2019-12-27 08:20:12 +0100

    CLI Run.

--- src/lib/cli/CLI.php
+++ src/lib/cli/CLI.php
@@ -2,9 +2,45 @@
 
 namespace CynoBit\PHPCLI;
 
+use Exception;
 use CynoBit\PHPCLI\Options;
 
 abstract class CLI
 {
+    /**
+     * Undocumented variable
+     *
+     * @var [type]
+     */
+    protected $options;
+
+    /**
+     * Undocumented function
+     */
+    function __construct()
+    {
+        $this->options = new Options();
+    }
+
+    /**
+     * Override setup to set up your CLI application options.
+     *
+     * @param  Options $options Options object that describes the operations and 
+     *                          parameters of your CLI application.
+     * @return void
+     */
     abstract protected function setup(Options $options): void;
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if ('cli' != php_sapi_name()) {
+            throw new Exception('This has to be run from the command line');
+        }
+        exit(0);
+    }
 }
