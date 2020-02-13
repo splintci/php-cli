28c15a09813ac22836afac0eeee62d4dce9adfa2
Francis Ilechukwu <francis94c@gmail.com>
2020-02-13 23:31:28 +0100

    Register Commands.

--- src/Splint.php
+++ src/Splint.php
@@ -1,17 +1,40 @@
 <?php
 
-namespace CynoBit\Splint;
+namespace CynoBit;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use CynoBit\PHPCLI\CLI;
 use CynoBit\PHPCLI\Options;
+use CynoBit\Splint\Commands\Install;
 
 class Splint extends CLI
 {
+    /**
+     * Undocumented function
+     *
+     * @param Options $options
+     * @return void
+     */
     protected function setup(Options $options): void
     {
+        $options->registerCommand('install', 'Install Splint Packages.');
+        $options->registerCommand('run', 'Run a Job');
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function main(Options $options): void
+    {
+        if ($options->getCommand() == 'install') {
+            (new Install())->run(array_merge($options->getOption('install') ? $options->getOption('install') : [], $options->getArgs()));
+        }
     }
 }
 
 $app = new Splint();
+
+$app->run();
